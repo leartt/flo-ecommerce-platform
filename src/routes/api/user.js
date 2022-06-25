a9cb0fe70eb59a49ad94b7e8c4e9367944195735
leartt <leartqeku@gmail.com>
2022-06-25 14:54:57 +0200

    add more api endpoints to user api

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 const UserController = require("../../controllers/user.controller");
 
+router.get("/", UserController.handleGetUsers);
 router.post("/", UserController.handleAddNewUser);
+router.get("/:id", UserController.handleGetUserById);
 
 module.exports = router;
