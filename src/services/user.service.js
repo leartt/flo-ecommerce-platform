d552d6c60e67430ce9e258142ecdafe65446116a
leartt <leartqeku@gmail.com>
2022-06-25 14:47:21 +0200

    remove throw error in user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,26 +3,17 @@ const ApplicationError = require("../utils/ApplicationError");
 
 const addNewUser = async (user) => {
   const newUser = new UserModel(user);
-  if (newUser.email === "test@test.test") {
-    throw new ApplicationError(401, "This email is not allowed");
-  }
   await newUser.save();
   return newUser;
 };
 
 const getUsers = async () => {
   const users = await UserModel.find({});
-  if (users.length === 0) {
-    throw new ApplicationError(404, "No users were found");
-  }
   return users;
 };
 
 const getUserById = async (id) => {
   const user = await UserModel.findOne({ _id: id });
-  if (!user) {
-    throw new ApplicationError(404, "No user was found");
-  }
   return user;
 };
 
