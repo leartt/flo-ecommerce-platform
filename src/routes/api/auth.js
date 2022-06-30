c3f1bb7e121036314d3ffd5fc503a37a8f2adaa0
leartt <leartqeku@gmail.com>
2022-06-30 17:22:57 +0200

    renew token endpoint handler

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -4,5 +4,6 @@ const AuthController = require("../../controllers/auth.controller");
 
 router.post("/login", AuthController.handleLogin);
 router.post("/signup", AuthController.handleSignup);
+router.post("/renew-token", AuthController.handleRenewToken);
 
 module.exports = router;
