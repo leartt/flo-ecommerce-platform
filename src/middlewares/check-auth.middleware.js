83b1545999f4921d7cfdf9c9ac508c4cbe7c77b0
leartt <leartqeku@gmail.com>
2022-06-30 17:24:22 +0200

    add authorization middleware for not allowing unauthorized requests

---
+++ src/middlewares/check-auth.middleware.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/user.model");
+const ApplicationError = require("../utils/ApplicationError");
+
+const checkAuth = async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) {
+      throw new ApplicationError(403, "Authorization header not provided");
+    }
+
+    const [bearer, token] = req.headers.authorization.split(" ");
+
+    if (bearer !== "Bearer") {
+      throw new ApplicationError(403, "Authorization token must be 'Bearer'");
+    }
+    if (!token) {
+      throw new ApplicationError(403, "No token provided");
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET_KEY);
+
+    const user = await UserModel.findOne({ _id: decoded._id });
+    if (!user) {
+      throw new ApplicationError(401, "Not authorized");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error.name === "ApplicationError") next(error);
+
+    next(new ApplicationError(401, error.message));
+  }
+};
+
+module.exports = checkAuth;
