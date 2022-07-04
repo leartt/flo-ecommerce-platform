ac780fd469f2da82eb5363d271e827ae9da4c987
leartt <leartqeku@gmail.com>
2022-07-05 00:02:10 +0200

    testing protected api endpoint

--- src/routes/api/product.js
+++ src/routes/api/product.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const ProductController = require("../../controllers/product.controller");
 
-router.get("/", ProductController.handleFindProducts);
+const checkAuth = require("../../middlewares/check-auth.middleware");
+
+router.get("/", checkAuth, ProductController.handleFindProducts);
 router.get("/:id", ProductController.handleFindProductById);
 router.post("/", ProductController.handleAddProduct);
 router.delete("/:id", ProductController.handleDeleteProductById);
