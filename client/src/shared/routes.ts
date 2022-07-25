e287cad3d5aac148ea3a7d5007c68d999f3ccfdd
leartt <leartqeku@gmail.com>
2022-07-25 18:29:33 +0200

    update on routes

--- client/src/shared/routes.ts
+++ client/src/shared/routes.ts
@@ -1,28 +1,19 @@
-import Home from '@src/pages/Home';
-import Shop from '@src/pages/Shop';
-import Contact from '@src/pages/Contact';
-import About from '@src/pages/About';
-
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
   },
   {
     path: '/shop',
     name: 'Shop',
-    component: Shop,
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: Contact,
   },
   {
     path: '/about',
     name: 'About',
-    component: About,
   },
 ];
 
