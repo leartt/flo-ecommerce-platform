c7b5446b4e2684bac4cea73bc1a19e63543bdcd1
leartt <leartqeku@gmail.com>
2022-07-25 16:37:40 +0200

    add featured product section

---
+++ client/src/components/FeaturedProductsSection/FeaturedProductSection.tsx
@@ -0,0 +1,43 @@
+import { Container, Grid } from '@mui/material';
+import React, { useEffect } from 'react';
+import useProductStore from '@src/stores/product';
+import useStyles from './styles';
+import FeaturedProductItem from '../FeaturedProductItem';
+
+const FeaturedProductSection = () => {
+  const products = useProductStore(state => state.products);
+  const isLoading = useProductStore(state => state.isLoading);
+  const message = useProductStore(state => state.message);
+  const getProducts = useProductStore(state => state.getProducts);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const classes = useStyles();
+
+  if (message.error) {
+    return <div>Something went wrong</div>;
+  }
+
+  return (
+    <div className={classes.featuredProducts}>
+      <Container>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            <h1 className={classes.title}>Featured Products</h1>
+            <Grid container>
+              {products?.slice(0, 3).map(product => (
+                <FeaturedProductItem key={product._id} product={product} />
+              ))}
+            </Grid>
+          </>
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default FeaturedProductSection;
---
+++ client/src/components/FeaturedProductsSection/styles.ts
@@ -0,0 +1,43 @@
+import { colors } from '@mui/material';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  featuredProducts: {
+    padding: '100px 0',
+    background: 'url(images/gradient-2-bg.jpeg)',
+    // backgroundSize: '100%',
+    backgroundPosition: 'top right',
+    backgroundRepeat: 'no-repeat',
+    position: 'relative',
+    zIndex: 1,
+    '&::after': {
+      content: "''",
+      position: 'absolute',
+      zIndex: -1,
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'rgba(0,0,0,0.2)',
+    },
+  },
+  title: {
+    textAlign: 'center',
+    color: colors.grey[100],
+    marginBottom: '50px',
+    fontWeight: 900,
+    position: 'relative',
+    '&::before': {
+      content: "''",
+      position: 'absolute',
+      bottom: -8,
+      left: '50%',
+      transform: 'translateX(-50%)',
+      width: 100,
+      height: 3,
+      background: colors.purple[500],
+    },
+  },
+});
+
+export default useStyles;
