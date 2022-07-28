036cea2a2e38d8a22891a6b5959e607bd6eb8ecf
leartt <leartqeku@gmail.com>
2022-07-28 16:10:43 +0200

    product card component displayed in shop page

---
+++ client/src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,116 @@
+import {
+  Button,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  colors,
+  Rating,
+} from '@mui/material';
+import { Product } from '@src/shared/interfaces/product.interface';
+import React from 'react';
+import { ShoppingCart } from '@mui/icons-material';
+import useCartStore from '@src/stores/cart';
+import NumberFormat from 'react-number-format';
+import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
+
+interface Props {
+  product: Product;
+  cardColumns: number;
+}
+
+const toFixedProductRating = (ratings: any[]) => {
+  const totalRatings =
+    ratings.reduce((acc: any, value: any) => acc + value, 0) / ratings.length;
+
+  return Number(totalRatings).toFixed(1);
+};
+
+const ProductCard = ({ product, cardColumns = 3 }: Props) => {
+  const addToCart = useCartStore(state => state.addToCart);
+  return (
+    <Grid item xs={12} sm={6} md={12 / cardColumns}>
+      <Link to={`/product/${product._id}`}>
+        <Card sx={{ cursor: 'pointer' }}>
+          <CardMedia
+            component="img"
+            height="200"
+            width="200"
+            image={`${import.meta.env.VITE_API_URL}${product.images[0].replace(
+              'src',
+              ''
+            )}`}
+            alt={product.title}
+            sx={{ objectFit: 'contain', background: '#fff', padding: '10px' }}
+          />
+          <CardContent sx={{ padding: '20px' }}>
+            <Typography sx={{ fontSize: 14, paddingTop: '10px' }} gutterBottom>
+              {product.name}
+            </Typography>
+            <Typography variant="h5">{product.title}</Typography>
+            <Typography
+              variant="body1"
+              sx={{ textTransform: 'capitalize' }}
+              gutterBottom
+            >
+              {product.color.value}
+            </Typography>
+            <Typography variant="h5" sx={{ padding: '10px 0' }}>
+              <NumberFormat
+                value={product.price}
+                prefix="$"
+                fixedDecimalScale
+                decimalScale={2}
+                thousandSeparator
+                displayType="text"
+              />
+            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Rating
+                name="product-rating"
+                value={+toFixedProductRating(product.ratings)}
+                precision={0.1}
+                readOnly
+                max={5}
+              />
+              <Typography variant="body1" marginLeft={1}>
+                {toFixedProductRating(product.ratings)}
+              </Typography>
+            </Box>
+          </CardContent>
+          <CardActions>
+            <Button
+              fullWidth
+              endIcon={<ShoppingCart />}
+              sx={{
+                borderColor: colors.purple[300],
+                backgroundColor: colors.purple[300],
+                padding: '10px 0',
+                color: '#fff',
+                '&:hover': {
+                  bgcolor: colors.purple[300],
+                  borderColor: colors.purple[300],
+                },
+              }}
+              size="medium"
+              variant="contained"
+              onClick={() => {
+                addToCart(product)
+                  .then(res => toast.success(res.message))
+                  .catch(err => toast.error(err));
+              }}
+            >
+              ADD TO CART
+            </Button>
+          </CardActions>
+        </Card>
+      </Link>
+    </Grid>
+  );
+};
+
+export default ProductCard;
