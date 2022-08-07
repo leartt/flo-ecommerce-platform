70c47b4c89fe3b95fff53195c9637e16b9efd818
leartt <leartqeku@gmail.com>
2022-08-08 01:16:14 +0200

    update route file to navigation links

---
+++ client/src/shared/navigation-links.ts
@@ -0,0 +1,47 @@
+export interface NavigationLinkType {
+  path: string;
+  name: string;
+  isAccountMenu?: boolean;
+}
+
+const baseNavigationLinks: NavigationLinkType[] = [
+  {
+    path: '/',
+    name: 'Home',
+  },
+  {
+    path: '/shop',
+    name: 'Shop',
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+  },
+  {
+    path: '/about',
+    name: 'About',
+  },
+];
+
+export const guestNavigationLinks: NavigationLinkType[] = [
+  ...baseNavigationLinks,
+  {
+    path: '/login',
+    name: 'Login',
+    isAccountMenu: true,
+  },
+  {
+    path: '/Signup',
+    name: 'Signup',
+    isAccountMenu: true,
+  },
+];
+
+export const authorizedNavigationLinks: NavigationLinkType[] = [
+  ...baseNavigationLinks,
+  {
+    path: '/my-profile',
+    name: 'My Profile',
+    isAccountMenu: true,
+  },
+];
