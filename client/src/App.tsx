055a7773a55228508a6f2eab88145e26b8fbd16f
leartt <leartqeku@gmail.com>
2022-08-07 22:54:32 +0200

    add more routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,47 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+
+import Header from '@src/components/Header';
 
 import Home from '@src/pages/Home';
 import About from '@src/pages/About';
 import Shop from '@src/pages/Shop';
 import Contact from '@src/pages/Contact';
+import Cart from '@src/pages/Cart';
+import Login from '@src/pages/Login';
+import Signup from '@src/pages/Signup';
 import Product from '@src/pages/Product';
-import Header from './components/Header';
+
+import ProtectedRoutes from '@src/ProtectedRoutes';
+import useAuthStore from '@src/stores/auth';
 
 /* eslint-disable */
 const App = () => {
+  const { getLoggedInUser, isLoggedIn } = useAuthStore();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      getLoggedInUser();
+    }
+  }, []);
+
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Route>
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/product/:id" element={<Product />} />
       </Routes>
+
       {/* used for displaying toast notification in every page */}
       <ToastContainer
         position="bottom-center"
