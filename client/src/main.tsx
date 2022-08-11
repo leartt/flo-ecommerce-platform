6ef82cdad2bcffe9e7d65d5af06e66d627ffc389
leartt <leartqeku@gmail.com>
2022-08-11 20:27:39 +0200

    remove strict mode, import react-cookie

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'swiper/css/bundle';
+import { CookiesProvider } from 'react-cookie';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <CookiesProvider>
     <App />
-  </React.StrictMode>
+  </CookiesProvider>
+  // </React.StrictMode>
 );
