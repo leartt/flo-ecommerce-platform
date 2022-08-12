cf782321186043eba9c5d9f6ec12633e863921b4
leartt <leartqeku@gmail.com>
2022-08-12 13:08:21 +0200

    add navigate state to protected route to redirect user to the page they was before

--- client/src/ProtectedRoutes.tsx
+++ client/src/ProtectedRoutes.tsx
@@ -1,11 +1,16 @@
-import { Outlet, Navigate } from 'react-router-dom';
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
 import useAuthStore from '@src/stores/auth';
 import { useEffect } from 'react';
 
 const ProtectedRoutes = () => {
+  const location = useLocation();
   const { isLoggedIn } = useAuthStore();
 
-  return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+  return isLoggedIn ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location.pathname }} />
+  );
 };
 
 export default ProtectedRoutes;
