58d299d1e2b8ba759ae54255414046ab0a11259d
leartt <leartqeku@gmail.com>
2022-08-13 20:05:11 +0200

    changes on checkout form

--- client/src/components/CheckoutForm/CheckoutForm.tsx
+++ client/src/components/CheckoutForm/CheckoutForm.tsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import { useStripe, useElements } from '@stripe/react-stripe-js';
-import { Alert, AlertTitle, Container, Typography } from '@mui/material';
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import CheckoutAddressSection from '@src/components/CheckoutAddressSection';
 import CheckoutPaymentSection from '@src/components/CheckoutPaymentSection';
+import CheckoutOrderSummarySection from '@src/components/CheckoutOrderSummarySection';
 import { UserShippingAddress } from '@src/shared/interfaces/user.interface';
 import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
 
 /* eslint-disable */
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
   const [error, setError] = useState<string | null | undefined>(null);
+  // const isTablet = useMediaQuery(useTheme().breakpoints.down('md'));
+
+  const classes = useStyles();
 
   const [chosenShippingAddress, setChosenShippingAddress] =
     useState<UserShippingAddress>();
 
-  const handlePaymentSubmit = async event => {
+  console.log('rendering checkout form');
+
+  const handlePaymentSubmit = async (event: any) => {
     // We don't want to let default form submission happen here,
     // which would refresh the page.
     event.preventDefault();
@@ -54,34 +72,72 @@ const CheckoutForm = () => {
       toast.error(result.error.message);
       console.log(result.error.message);
       setError(result.error.message);
-    } else {
-      // The payment has been processed!
-      // if (result === 'succeeded') {
-      console.log(result);
-      // Show a success message to your customer
-      // There's a risk of the customer closing the window before callback
-      // execution. Set up a webhook or plugin to listen for the
-      // payment_intent.succeeded event that handles any business critical
-      // post-payment actions.
-      // }
     }
   };
 
   return (
     <Container>
-      <Typography variant="h5">Checkout</Typography>
-      <CheckoutAddressSection
-        setChosenShippingAddress={setChosenShippingAddress}
-        chosenShippingAddress={chosenShippingAddress}
-      />
-      <CheckoutPaymentSection handlePaymentSubmit={handlePaymentSubmit}>
-        {error && (
-          <Alert severity="error">
-            <AlertTitle>Error</AlertTitle>
-            {error}
-          </Alert>
-        )}
-      </CheckoutPaymentSection>
+      <Typography variant="h4">Checkout</Typography>
+      <Grid container spacing={3} paddingY={2}>
+        {/* checkout-form */}
+        <Grid
+          component={'form'}
+          onSubmit={handlePaymentSubmit}
+          item
+          xs={12}
+          md={8}
+          className={classes.checkoutForm}
+        >
+          <CheckoutAddressSection
+            setChosenShippingAddress={setChosenShippingAddress}
+            chosenShippingAddress={chosenShippingAddress}
+          />
+          <CheckoutPaymentSection /* handlePaymentSubmit={handlePaymentSubmit} */
+          >
+            {error && (
+              <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                {error}
+              </Alert>
+            )}
+          </CheckoutPaymentSection>
+          {/* payment can be made either by clicking button here 
+          or wrapping <PaymentElements/> inside a form on the CheckoutPaymentSection component */}
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Button variant="outlined" color="info" component={Link} to="/cart">
+              Back to cart
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={!stripe || !elements}
+              type="submit"
+              // sx={{
+              //   display: 'block',
+              //   marginLeft: 'auto',
+              //   minWidth: 'fit-content',
+              //   maxWidth: '200px',
+              //   width: '100%',
+              // }}
+            >
+              Place order
+            </Button>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={4}
+          order={1}
+          className={classes.checkoutOrderSummary}
+        >
+          <CheckoutOrderSummarySection />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
