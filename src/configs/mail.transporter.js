f58c412b3ce34d734507830b55c8719e0c9ff10b
leartt <leartqeku@gmail.com>
2022-11-18 15:00:55 +0100

    add nodemailer for sending emails

---
+++ src/configs/mail.transporter.js
@@ -0,0 +1,13 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASSWORD,
+  },
+});
+
+module.exports = transporter;
