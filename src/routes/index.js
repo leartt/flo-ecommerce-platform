1359283170f0b4aa9be6401e0bf17d029fb8e5b4
leartt <leartqeku@gmail.com>
2022-11-18 15:08:40 +0100

    update api routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
 
-const productApi = require("./api/product");
 const userApi = require("./api/user");
 const authApi = require("./api/auth");
+const productApi = require("./api/product");
+const orderApi = require("./api/order");
+const invoiceApi = require("./api/invoice");
+const couponApi = require("./api/coupon");
+const stripeApi = require("./api/stripe");
 
-router.use("/products", productApi);
 router.use("/users", userApi);
 router.use("/auth", authApi);
+router.use("/products", productApi);
+router.use("/coupons", couponApi);
+router.use("/orders", orderApi);
+router.use("/invoice", invoiceApi);
+router.use("/stripe", stripeApi);
 
 module.exports = router;
